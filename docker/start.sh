94f9ef44cc7256fffee7e0c5a31f88008dd5b007
Espoir Mukongo <espoirmmuk@gmail.com>
2026-08-27 12:29:19 +0100

    Configuration SSL Aiven pour Render

--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,15 @@ set -e
 
 echo "Démarrage de Laravel..."
 
+# Créer le certificat SSL Aiven à partir de la variable Render
+if [ -n "$AIVEN_CA_CERT" ]; then
+    echo "$AIVEN_CA_CERT" > /etc/ssl/certs/aiven-ca.pem
+    chmod 644 /etc/ssl/certs/aiven-ca.pem
+    echo "Certificat SSL Aiven installé."
+else
+    echo "ATTENTION : AIVEN_CA_CERT n'est pas défini."
+fi
+
 php artisan config:clear
 php artisan cache:clear
 php artisan view:clear
